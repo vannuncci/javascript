92add11af2d50ad50d83019f20fbb92218c5344e
vannuncci <edufiscal@yahoo.com.br>
2023-07-22 13:30:59 -0300

    Ajuste resultado

--- Aula_9/app1.js
+++ Aula_9/app1.js
@@ -1,25 +1,26 @@
 // verificar se o input é maior que 10
 
 const buttonElement = document.getElementById('checkButton');
+const resultadoMensagemElement = document.getElementById('resultadoMensagem');
 buttonElement.addEventListener('click', 
     function() { 
         var inputValue = document.getElementById('numeroInput').value; 
 
         if (inputValue == '' || isNaN(inputValue)) {
             alert('O valor digitado não é um número válido!');
-            buttonElement.classList.remove('button-red');
-            buttonElement.classList.remove('button-green'); 
-            buttonElement.innerHTML = 'Verificar'; 
+            resultadoMensagemElement.classList.remove('button-red');
+            resultadoMensagemElement.classList.remove('button-green');  
+            resultadoMensagemElement.innerHTML = '';
             return;
         } else if (inputValue > 10) {   
-            buttonElement.classList.remove('button-red');  
-            buttonElement.classList.add('button-green');  
-            buttonElement.innerHTML = 'O valor  ' + inputValue + '  é maior que 10!'; 
+            resultadoMensagemElement.classList.remove('button-red');  
+            resultadoMensagemElement.classList.add('button-green');  
+            resultadoMensagemElement.innerHTML = 'O valor  ' + inputValue + '  é maior que 10!'; 
             
         } else { 
-            buttonElement.classList.remove('button-green');  
-            buttonElement.classList.add('button-red');  
-            buttonElement.innerHTML = 'O valor ' + inputValue + ' é menor que 10!';  
+            resultadoMensagemElement.classList.remove('button-green');  
+            resultadoMensagemElement.classList.add('button-red');  
+            resultadoMensagemElement.innerHTML = 'O valor ' + inputValue + ' é menor que 10!';  
             
         }
     });
