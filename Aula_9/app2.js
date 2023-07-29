09d07a9b5089559ec800659cbdac6a24e1f409f6
vannuncci <edufiscal@yahoo.com.br>
2023-07-29 12:32:59 -0300

    Retirada if...

--- Aula_9/app2.js
+++ Aula_9/app2.js
@@ -14,33 +14,26 @@ buttonElement.addEventListener('click',
         // contar quantas tentativas o usuario fez
         tentativas++;
         // criar numero aleatorio de 1 a 100
-        if (inputValue == '' || isNaN(inputValue)) { 
+        if (inputValue == '' || isNaN(inputValue) || (inputValue > 100 || inputValue < 1)) { 
             // se o numero digitado for vazio ou nao for um numero mostrar mensagem de numero invalido em vermelho.
             resultadoMensagemElement.classList.add('result-error');
             resultadoMensagemElement.innerHTML = 'O valor digitado não é um número válido!';
             return;
-        } else {
-            // se o numero for maior que 100 ou menor que 1 mostrar mensagem de numero invalido em vermelho.
-            if (inputValue > 100 || inputValue < 1) {  
-                resultadoMensagemElement.classList.add('result-error');
-                resultadoMensagemElement.innerHTML = 'O valor digitado não é um <strong> número </strong> válido! <br> Tentativa ' + tentativas + '.';
-                // verificar se o numero digitado é igual ao numero sorteado
-            } else if (inputValue == numeroAleatorio) {
-                    // se o numero digitado for igual ao numero sorteado mostrar mensagem de parabens em verde.
-                    resultadoMensagemElement.classList.add('result-success');
-                    resultadoMensagemElement.innerHTML = '<strong> Parabéns!</strong>  Você acertou o número sorteado! <br> Tentativa ' + tentativas + '.'
-                    // chamar funcao para salvar txt com a mensagem usando CreateTextFile
-                    saveTextAsFile();
-                } else if (inputValue > numeroAleatorio) {
-                    // verificar se o numero digitado é maior que o numero sorteado 
-                    resultadoMensagemElement.classList.add('result-fail');  
-                    resultadoMensagemElement.innerHTML = 'O valor  ' + inputValue + '  é <strong> maior </strong> que o número sorteado! <br> Tentativa ' + tentativas + '.' 
-                } else if (inputValue < numeroAleatorio) {
-                    // verificar se o numero digitado é  menor que o numero sorteado
-                    resultadoMensagemElement.classList.add('result-fail');  
-                    resultadoMensagemElement.innerHTML = 'O valor  ' + inputValue + '  é <strong> menor </strong> que o número sorteado! <br> Tentativa ' + tentativas + '.'
-                }
-        } 
+        } else if (inputValue == numeroAleatorio) {
+            // se o numero digitado for igual ao numero sorteado mostrar mensagem de parabens em verde.
+            resultadoMensagemElement.classList.add('result-success');
+            resultadoMensagemElement.innerHTML = '<strong> Parabéns!</strong>  Você acertou o número sorteado! <br> Tentativa ' + tentativas + '.'
+            // chamar funcao para salvar txt com a mensagem usando CreateTextFile
+            saveTextAsFile();
+        } else if (inputValue > numeroAleatorio) {
+            // verificar se o numero digitado é maior que o numero sorteado 
+            resultadoMensagemElement.classList.add('result-fail');  
+            resultadoMensagemElement.innerHTML = 'O valor  ' + inputValue + '  é <strong> maior </strong> que o número sorteado! <br> Tentativa ' + tentativas + '.' 
+        } else if (inputValue < numeroAleatorio) {
+            // verificar se o numero digitado é  menor que o numero sorteado
+            resultadoMensagemElement.classList.add('result-fail');  
+            resultadoMensagemElement.innerHTML = 'O valor  ' + inputValue + '  é <strong> menor </strong> que o número sorteado! <br> Tentativa ' + tentativas + '.'
+        }
     });
 
     // criar funcao para salvar txt com a mensagem usando CreateTextFile
